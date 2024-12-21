d8ac537931a5c35d5bae2bb65f577e89db46ca4a
bridgelessqiu <zq5au@virginia.edu>
2024-12-21 15:18:08 -0500

    New problem

---
+++ beautiful_year.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int year;
+    cin >> year;
+    
+    // --- Update ---
+    bool flag = true;
+    int mask, last_digit, temp_year, bit;
+
+    while (flag)
+    {
+        year++;
+
+        temp_year = year;
+        mask = 0;
+        flag = false;
+
+        while (temp_year)
+        {
+            last_digit = temp_year % 10; 
+            bit = (1 << last_digit); 
+
+            if (mask & bit)
+            {
+                flag = true;
+                break;
+            }
+
+            temp_year /= 10;
+            mask |= bit;
+        }
+    }
+
+    cout<<year<<endl;
+    
+    return 0;
+}
